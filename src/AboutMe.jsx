fb8c1ac9a6c811cff4108fc6bc30a5062b68a408
ol-dychka <ol.dychka@gmail.com>
2025-03-08 17:52:13 -0500

    fix: links and bio updated

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -20,19 +20,28 @@ export default function AboutMe() {
         </p>
       </div>
       {open && (
-        <p className="text-lg font-mono text-slate-500 max-w-3xl">
-          Hello! I’m Alex, a junior Front-End developer based in Toronto,
-          Canada. Originally from Ukraine, I moved here after studying Computer
-          Science for two years at a top university back home. Though I couldn’t
-          complete my degree, I continued developing my skills by building small
-          projects to enhance my resume. <br /> The idea for this extension came
-          from a friend who noticed the lack of good, affordable ChatGPT
-          organizers. Most were either poorly made or expensive. I set out to
-          create a quality product at a reasonable price, but just before
-          finishing, OpenAI announced a similar feature for free. Instead of
-          letting my work go to waste, I modified the code and decided to
-          release it for free. Hope you find it useful!
-        </p>
+        <div className="text-lg font-mono text-slate-500 max-w-3xl space-y-4">
+          <p>
+            Hello! I’m Alex, a junior Front-End developer based in Toronto,
+            Canada. Originally from Ukraine, I moved here with my mother after
+            studying Computer Science for two years at a university back home.
+          </p>
+          <p>
+            Though I couldn’t complete my degree, I continued developing my
+            skills by building small projects to enhance my resume.
+          </p>
+          <p>
+            The idea for this extension came from a good friend of mine who
+            noticed the lack of good, affordable ChatGPT organizers. Most were
+            either poorly made or expensive. I set out to create a quality
+            product at a reasonable price, but just before finishing, OpenAI
+            announced a similar feature for free.
+          </p>
+          <p>
+            Instead of letting my work go to waste, I modified the code and
+            decided to release it for free. Hope you find it useful!
+          </p>
+        </div>
       )}
     </div>
   );
--- src/Extension.jsx
+++ src/Extension.jsx
@@ -1,4 +1,3 @@
-import PayPal from "./assets/paypal.png";
 import WebStore from "./assets/webstore.svg";
 
 const Extension = () => {
@@ -6,20 +5,23 @@ const Extension = () => {
     <div className="bg-white flex justify-around font-medium p-24 text-slate-700 text-xl shadow-xl">
       <div className="flex gap-4 items-center">
         <img src={WebStore} alt="extension icon" className=" h-12 w-12" />
-        <a href="" target="_blank" className="hover:text-slate-900">
-          My extension in Chrome Web Store
-        </a>
-      </div>
-      <div className="flex gap-4 items-center">
-        <img src={PayPal} alt="extension icon" className=" h-12 w-12" />
         <a
-          href="https://www.paypal.com/ncp/payment/AMMT6K9L6ZG76"
+          href="https://chromewebstore.google.com/detail/chatgpt-folders/akgpkjgpdobkagnipbohfpagjmbpjnfb"
           target="_blank"
           className="hover:text-slate-900"
         >
-          Contribute a small donation w/ PayPal
+          My extension in Chrome Web Store
         </a>
       </div>
+      <a href="https://ko-fi.com/G2G51BOHXW" target="_blank">
+        <img
+          height="36"
+          className="h-12"
+          src="https://storage.ko-fi.com/cdn/kofi3.png?v=6"
+          border="0"
+          alt="Buy Me a Coffee at ko-fi.com"
+        />
+      </a>
     </div>
   );
 };
